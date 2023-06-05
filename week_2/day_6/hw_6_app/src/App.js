647474282746e650c988825337f561b7ea74a63f
skojr <133845711+skojr@users.noreply.github.com>
2023-06-05 19:32:05 -0400

    hw-6

---
+++ week_2/day_6/hw_6_app/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { useState } from 'react';
+import BookForm from './components/BookForm';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BookTable from './components/BookTable';
+
+
+function App() {
+
+  const [books, setBooks] = useState([]);
+  const [editBook, setBookToEdit] = useState(null);
+  
+  function onBookCreated(book) {
+    setBookToEdit(null);
+    setBooks([...books, book]);
+  }
+
+  function onBookDelete(book) {
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+  }
+
+  function onBookEdit(book) {
+    setBookToEdit(book);
+    setBooks(books.filter((x) => x.isbn !== book.isbn));
+    console.log('register');
+  }
+
+  return (
+    <div className='container my-5'>
+      <div className='card p-4'>
+        <BookForm onBookCreated={onBookCreated} editBook={editBook}></BookForm>
+        <BookTable books={books} onBookDelete={onBookDelete} onBookEdit={onBookEdit}></BookTable>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ week_2/day_6/hw_6_app/src/components/BookForm.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import { Book } from '../models/book';
+
+export default function BookForm(props) {
+
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isbn, setIsbn] = useState('');
+
+    function onBookFormSubmut(e) {
+        e.preventDefault();
+
+        let book = new Book(title, author, isbn);
+        props.onBookCreated(book);
+        clearInputs();
+    }
+
+    function clearInputs() {
+        setTitle('');
+        setAuthor('');
+        setIsbn('');
+
+    }
+
+    useEffect(() => {
+        if (props.editBook) {
+          setTitle(props.editBook.title);
+          setAuthor(props.editBook.author);
+          setIsbn(props.editBook.isbn);
+        }
+      }, [props.editBook]);
+
+  return (
+    <div>
+        <h1>Library</h1>
+
+        <form id="form" onSubmit={onBookFormSubmut}>
+          <div className="mb-3">
+            <label className="form-label"> Title </label>
+            <input id="title-input" type="text" className="form-control" onChange={(e) => setTitle(e.target.value)} value={title}/>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label"> Author </label>
+            <input id="author-input" type="text" className="form-control" onChange={(e) => setAuthor(e.target.value)} value={author}/>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label"> #ISBN </label>
+            <input id="isbn-input" type="text" className="form-control" onChange={(e) => setIsbn(e.target.value)} value={isbn}/>
+          </div>
+
+          <div className="d-grid mt-4">
+            <button className="btn btn-outline-primary" type="submit">
+                {props.editBook ? 'Update Book' : 'Add Book'}
+            </button>
+          </div>
+        </form>
+    </div>
+  )
+}
